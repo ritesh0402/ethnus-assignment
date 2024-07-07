d6740b49b0b5fe5ee9ddd6988373c2dcc3fa8d7a
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2024-07-07 21:57:26 +0530

    Initializes typescript React application

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppRoutes from './components/Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
